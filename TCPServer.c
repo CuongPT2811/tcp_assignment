fc4f86081c48ab56ad0ddeec62c043dd021b0658
CuongPT24 <cuongpt24@fpt.com>
2025-06-03 21:11:32 +0700

    Update pattern validation for username and password functions

--- TCPServer.c
+++ TCPServer.c
@@ -9,11 +9,19 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#include <regex.h>
+#include <ctype.h>
+
+#define USERNAME_PATTERN "^[A-Za-z0-9!@#$%^&*?.\\/]{6,50}$"
+#define PASSWORD_PATTERN "^[A-Za-z0-9!@#$%^&*?.\\/]{6,50}$"
+
 #define MAX_LENGTH 1024
 #define PORT 5555
 #define SA struct sockaddr
 
 void communicate_function(int client_fd);
+int validate_pattern(const char *input, const char *pattern);
+int validate_password(const char *password);
 
 int main()
 {
@@ -104,6 +112,35 @@ void communicate_function(int client_fd)
     }
 }
 
-void chat_menu(){
+int validate_password(const char *password)
+{
+    int has_upper = 0;
+    int has_lower = 0;
+    int has_digit = 0;
+    int has_special = 0;
+    for (int i = 0; password[i]; i++)
+    {
+        if (isupper(password[i])) has_upper = 1;
+        else if (islower(password[i])) has_lower = 1;
+        else if (isdigit(password[i])) has_digit = 1;
+        else if (strchr("!@#$%^&*?.\\/", password[i])) has_special = 1;
+    }
 
+    return has_upper && has_lower && has_digit && has_special;
 }
+
+int validate_pattern(const char *input, const char *pattern)
+{
+    regex_t regex;
+    int result;
+    result = regcomp(&regex, pattern, REG_EXTENDED);
+    if(result != 0)
+    {
+        fprintf(stderr, "Regex could not be compiled\n");
+        return 0;
+    }
+    result = regexec(&regex, input, 0, NULL, 0);
+    regfree(&regex);
+    return !result;
+}
+
